396f063e716428061cd8731ecd3fb8de95615b46
Ashim Usmani <ashimusmani@gmail.com>
2019-08-01 02:22:51 +0530

    added backend integration for create user functionality

--- src/Users/CreateUser.js
+++ src/Users/CreateUser.js
@@ -2,7 +2,7 @@ import React, { Component, useContext } from 'react';
 import { Form, Icon, Input, Button } from 'antd';
 import { Row, Col, message, Spin, Select } from 'antd';
 import axios from 'axios';
-import UserContext from './UserContext';
+import UserContext from '../UserContext';
 
 const Option = Select.Option;
 const FormItem = Form.Item;
@@ -26,9 +26,9 @@ class CreateUser extends Component {
             iconLoading: false,
             userName: '',
             password: '',
-            confirmPassword: '',
-            firstname: '',
-            lastname: '',
+            displayName: '',
+            firstName: '',
+            lastName: '',
             email: '',
             role: '',
         }
@@ -36,44 +36,33 @@ class CreateUser extends Component {
 
     componentDidMount() {
         document.title = "Create User";
-        console.log(this.context);
+        console.log("user context: ", this.context);
     }
 
     changeRole = (selectedRole) => {
         this.setState({ role: selectedRole });
     }
 
-    validateEmail = (email) => {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (re.test(email)) {
-            return true;
-        } else {
-            message.error('Please provide email address to continue!', 5);
-            return false;
-        }
-    }
-
     createUser = () => {
-
         this.setState({ "iconLoading": true });
-
         var data = {
-            'username': this.state.userName,
+            'userName': this.state.userName,
             'password': this.state.password,
             'enabled': true,
-            'firstname': this.state.firstname,
-            'lastname': this.state.lastname,
+            'displayName': this.state.displayName,
+            'firstName': this.state.firstName,
+            'lastName': this.state.lastName,
             'email': this.state.email,
-            'role': this.state.role,
+            'roles': [this.state.role],
         };
-        axios.post(  'http://localhost:8097/user1/', data)
+        axios.post('http://localhost:8097/v1/user/', data)
             .then((response) => {
                 console.log(response);
                 this.setState({ "iconLoading": false });
                 message.success('User created successfully.', 5);
+                this.props.history.push('/profile/manage-users');
             })
             .catch((error) => {
-                //code comes here if status code greater than 2xx
                 this.setState({ iconLoading: false });
             });
     }
@@ -81,19 +70,19 @@ class CreateUser extends Component {
     render() {
         return (
             <div style={{ minHeight: 'calc(100vh - 64px)' }}>
+                <Row type="flex" justify="center" align="middle" style={{ paddingTop: '2px', paddingBottom: '4px' }}>
+                    <Col span={24}>
+                        <label style={{ fontWeight: 'bold' }} >Create New User</label>
+                        <span>&nbsp;&nbsp;</span>
+                        <Spin spinning={this.state.iconLoading} />
+                    </Col>
+                </Row>
                 <Row type="flex" justify="center" align="middle">
-                    <Col span={20}  >
-
+                    <Col span={24}  >
                         <Form style={{ backgroundColor: 'white' }}>
-
-                            <label style={{ fontSize: 15, fontWeight: 'bold' }}>Create New User</label>
-                            <br />
-                            <Spin spinning={this.state.iconLoading} />
-                            <br />
-
-                            <FormItem {...formItemLayout} label="Username:">
+                            <FormItem {...formItemLayout} label="User Name:">
                                 <Input style={{ fontSize: 20 }} prefix={<Icon type="edit" style={{ fontSize: 20 }} />}
-                                    placeholder=" Username"
+                                    placeholder=" User Name"
                                     value={this.state.userName}
                                     onChange={(e) => { this.setState({ userName: e.target.value }) }} />
                             </FormItem>
@@ -101,31 +90,27 @@ class CreateUser extends Component {
                                 <Input.Password style={{ fontSize: 20 }} prefix={<Icon type="lock" style={{ fontSize: 20 }} />}
                                     type="password" placeholder=" Password"
                                     value={this.state.password}
-                                    onBlur={this.validatePassword}
                                     onChange={(e) => {
                                         this.setState({ password: e.target.value })
                                     }} />
                             </FormItem>
-                            <FormItem {...formItemLayout} label="Confirm Password:">
-                                <Input.Password style={{ fontSize: 20 }} prefix={<Icon type="lock" style={{ fontSize: 20 }} />}
-                                    type="password" placeholder=" Confirm Password"
-                                    value={this.state.confirmPassword}
-                                    onBlur={this.validatePassword}
-                                    onChange={(e) => {
-                                        this.setState({ confirmPassword: e.target.value })
-                                    }} />
+                            <FormItem {...formItemLayout} label="Display Name:">
+                                <Input style={{ fontSize: 20 }} prefix={<Icon type="edit" style={{ fontSize: 20 }} />}
+                                    placeholder=" Display Name"
+                                    value={this.state.displayName}
+                                    onChange={(e) => { this.setState({ displayName: e.target.value }) }} />
                             </FormItem>
                             <FormItem {...formItemLayout} label="First Name:">
                                 <Input style={{ fontSize: 20 }} prefix={<Icon type="edit" style={{ fontSize: 20 }} />}
                                     placeholder=" First Name"
-                                    value={this.state.firstname}
-                                    onChange={(e) => { this.setState({ firstname: e.target.value }) }} />
+                                    value={this.state.firstName}
+                                    onChange={(e) => { this.setState({ firstName: e.target.value }) }} />
                             </FormItem>
                             <FormItem {...formItemLayout} label="Last Name:">
                                 <Input style={{ fontSize: 20 }} prefix={<Icon type="edit" style={{ fontSize: 20 }} />}
                                     placeholder=" Last Name"
-                                    value={this.state.lastname}
-                                    onChange={(e) => { this.setState({ lastname: e.target.value }) }} />
+                                    value={this.state.lastName}
+                                    onChange={(e) => { this.setState({ lastName: e.target.value }) }} />
                             </FormItem>
                             <FormItem {...formItemLayout} label="Email:">
                                 <Input style={{ fontSize: 20 }} prefix={<Icon type="edit" style={{ fontSize: 20 }} />}
